fba214e7420001bc88e3787c25dca1ac31b1426e
Jordy <jordysp92@gmail.com>
2018-11-13 21:35:15 -0300

    Se agrega variable de entorno mongodb

--- server/config/config.js
+++ server/config/config.js
@@ -14,6 +14,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev')
     urlDB = 'mongodb://localhost:27017/cafe';
 else
-    urlDB = 'mongodb://cafe-user:123456a@ds161183.mlab.com:61183/cafe'
+    urlDB = process.env.MONGO_DB_URL;
 
 process.env.URLDB = urlDB;
